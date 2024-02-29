d07804ed7dc267cc093cf3dd5932d32353534016
David S. Alexander <davidsobhysabet@gmail.com>
2024-02-29 02:58:29 +0200

    Update PduR_Com.c

--- PduR_Com.c
+++ PduR_Com.c
@@ -18,23 +18,112 @@
 
 
 /*******************************************************************************
- *                           Functions Declaration                             *
+ *                           Functions Implementation                          *
  *******************************************************************************/
 
-Std_ReturnType PduR_ComTransmit (PduIdType TxPduId, const PduInfoType* PduInfoPtr)
+/**
+ * @brief Transmits a PDU (Protocol Data Unit) from the upper layer to a destination module.
+ *
+ * This function routes a PDU identified by TxPduId from the upper layer to its destination module.
+ * The PDU data is provided through the PduInfoPtr parameter.
+ *
+ * @param TxPduId The ID of the PDU to be transmitted.
+ * @param PduInfoPtr Pointer to a structure containing the PDU data and its length.
+ * @return Std_ReturnType E_OK if the PDU transmission is successful, E_NOT_OK otherwise.
+ */
+Std_ReturnType PduR_ComTransmit(PduIdType TxPduId, const PduInfoType* PduInfoPtr)
 {
-	Std_ReturnType ret = E_OK;
-    if( PDUR_UNINIT == PduRState )
+    /* Initialize return value to indicate failure by default */
+    Std_ReturnType ret = E_NOT_OK;
+
+    /* Check if the PDU Router module is initialized */
+    if (PDUR_UNINIT == PduRState)
     {
-    	Det_ReportError(PDUR_MODULE_ID, PDUR_INSTANCE_ID, PDUR_COM_TRANSMIT_SID, PDUR_E_UNINIT);
+        /* Report error if PDU Router module is not initialized */
+#if (PDUR_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PDUR_MODULE_ID, PDUR_INSTANCE_ID, PDUR_COM_TRANSMIT_SID, PDUR_E_UNINIT);
+#endif
+        /* Return error */
+        ret = E_NOT_OK;
     }
-    else if( PduInfoPtr == NULL_PTR )
+    /* Check if PduInfoPtr is NULL */
+    else if (PduInfoPtr == NULL_PTR)
     {
-    	Det_ReportError(PDUR_MODULE_ID, PDUR_INSTANCE_ID, PDUR_COM_TRANSMIT_SID, PDUR_E_PARAM_POINTER);
+        /* Report error if PduInfoPtr is NULL */
+#if (PDUR_DEV_ERROR_DETECT == STD_ON)
+        Det_ReportError(PDUR_MODULE_ID, PDUR_INSTANCE_ID, PDUR_COM_TRANSMIT_SID, PDUR_E_PARAM_POINTER);
+#endif
+        /* Return error */
+        ret = E_NOT_OK;
     }
     else
-	{
+    {
+        /* Iterate through each routing path to find the matching TxPduId */
+        for (uint8_t PathsCounter = 0; PathsCounter < PBPduRConfig.PduRRoutingPaths.PduRMaxRoutingPathCnt; PathsCounter++)
+        {
+            /* Get the current routing path */
+            const PduRRoutingPath_type* RoutingPath = &PBPduRConfig.PduRRoutingPaths.PduRRoutingPath[PathsCounter];
 
-	}
+            /* Check if the current routing path matches the TxPduId */
+            if (RoutingPath->PduRSrcPduRef->PduRSourcePduHandleId == TxPduId)
+            {
+                /* Check the data provision type for the destination PDU */
+                if (RoutingPath->PduRDestPduRRef->PduRDestPduDataProvision == PDUR_DIRECT)
+                {
+                    /* If data provision is direct, transmit the PDU directly */
+
+               /* 	switch (RoutingPath->PduRDestPduRRef->PduR_Module)
+                	{
+                	   case PDUR_CAN_IF:
+                	        ret = CanIf_Transmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                	        break;
+                	   case PDUR_LIN_IF:
+                	        ret = LinIf_Transmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                	        break;
+                   	   case PDUR_FR_IF:
+                   	        ret = FrIf_Transmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                   	        break;
+                   	   case PDUR_FR_NM:
+                   	        ret = LinIf_Transmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                   	       break;
+                   	   default:
+                   		   break;
+                	}     */
+                }
+                else if (RoutingPath->PduRDestPduRRef->PduRDestPduDataProvision == PDUR_TRIGGERTRANSMIT)
+                {
+                    /* If data provision involves triggered transmission, trigger the transmission */
+
+                    /* switch (RoutingPath->PduRDestPduRRef->PduR_Module)
+                       {
+                     	  case PDUR_CAN_IF:
+                     	       ret = CanIf_TriggerTransmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                     	       break;
+                     	  case PDUR_LIN_IF:
+                     	       ret = LinIf_TriggerTransmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                     	       break;
+                          case PDUR_FR_IF:
+                               ret = FrIf_TriggerTransmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                               break;
+                          case PDUR_FR_NM:
+                               ret = LinIf_TriggerTransmit(RoutingPath->PduRDestPduRRef->PduRDestPduHandleId, PduInfoPtr);
+                               break;
+                          default:
+                               break;
+                     	}     */
+                }
+                else
+                {
+                    /* Unsupported data provision type, break the loop */
+                    break;
+                }
+                /* Break the loop after processing the matching routing path */
+                break;
+            }
+        }
+    }
+
+    /* Return the transmission result */
     return ret;
 }
+
